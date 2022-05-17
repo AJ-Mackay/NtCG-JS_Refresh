19ef5c4e436e848cc0f5c77369d9e23bd05e63e4
Paul Mackay <paul.m.mackay85@gmail.com>
2022-05-17 13:51:26 +0100

    Arrays, Objects & Reference Types

--- play.js
+++ play.js
@@ -35,11 +35,13 @@ const person = {
   },
 };
 
-person.greet();
+// person.greet();
 
 const hobbies = ['Reading', 'Puzzles'];
 // for (let hobby of hobbies) {
 //   console.log(hobby);
 // }
-console.log(hobbies.map((hobby) => 'Hobby: ' + hobby));
+// console.log(hobbies.map((hobby) => 'Hobby: ' + hobby));
+// console.log(hobbies);
+hobbies.push('Programming');
 console.log(hobbies);
