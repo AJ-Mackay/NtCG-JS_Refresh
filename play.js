f1648bf6e7a331797f282124d70546d3e29a47fd
Paul Mackay <paul.m.mackay85@gmail.com>
2022-05-17 14:05:33 +0100

    Destructuring

--- play.js
+++ play.js
@@ -35,22 +35,34 @@ const person = {
   },
 };
 
+const printName = ({ name }) => {
+  console.log(name);
+};
+
+printName(person);
+
+const { name, age } = person;
+console.log(name, age);
+
 // person.greet();
-const copiedPerson = { ...person };
-console.log(copiedPerson);
+// const copiedPerson = { ...person };
+// console.log(copiedPerson);
 
 const hobbies = ['Reading', 'Puzzles'];
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1, hobby2);
+
 // for (let hobby of hobbies) {
 //   console.log(hobby);
 // }
 // console.log(hobbies.map((hobby) => 'Hobby: ' + hobby));
 // console.log(hobbies);
 // hobbies.push('Programming');
-const copiedArray = [...hobbies];
-console.log(copiedArray);
+// const copiedArray = [...hobbies];
+// console.log(copiedArray);
 
-const toArray = (...args) => {
-  return args;
-};
+// const toArray = (...args) => {
+//   return args;
+// };
 
-console.log(toArray(1, 2, 3, 4));
+// console.log(toArray(1, 2, 3, 4));
